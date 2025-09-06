12f4b20ce5e967c8da0d1cdc6928e0193a258f3d
JuninhoSouza25 <dnt-bh@hotmail.com>
2025-09-06 13:17:46 -0300

    Remove componente Whatsapp do layout

--- app/layout.js
+++ app/layout.js
@@ -89,7 +89,7 @@ export default function RootLayout({ children }) {
         <ScrollToTop />
         {children}
         <BackToTop />
-        <WhatsappComponent />
+        {/* <WhatsappComponent /> */}
         <script 
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" 
           integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
